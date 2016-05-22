3d4538931fe06b26587d8a23fab52ac16f441853
Nick Rucci <ld.tech@outlook.com>
2016-05-21 23:16:48 -0600

    generate first test sweet for Go

--- lib/marigold.rb
+++ lib/marigold.rb
@@ -1,3 +1,26 @@
 Dir.glob("./*.go") do |file|
-  puts "working on: #{file}..."
+  base = File.basename(file, File.extname(file))
+  File.open(base + "_test.go", "w") { |f| 
+    # model name config
+    upcase_name = base.slice(0,1).capitalize + base.slice(1..-1)
+
+    f.write('package main_test')
+    f.puts @string
+
+    f.write('import (')
+    f.puts @string
+    f.write('. "github.com/onsi/ginkgo"')
+    f.puts @string
+    f.write('. "github.com/onsi/gomega"')
+    f.puts @string
+    f.write(')')
+    f.puts @string
+
+    f.write('var _ = Describe("'+upcase_name+'", func() {')
+    f.puts @string
+    f.puts @string
+    f.write('})')
+  }
+
+  system "go fmt"
 end
--- marigold.gemspec
+++ marigold.gemspec
@@ -1,9 +1,10 @@
 Gem::Specification.new do |s|
   s.name        = 'marigold'
-  s.version     = '0.0.0'
+  s.version     = '0.0.1'
   s.date        = '2016-05-21'
   s.summary     = "Test Code Generation"
   s.description = "Marigold is a script built for Go that generates basic CRUD test for each file in the current directory within an application"
+  s.executables = "marigold"
   s.authors     = ["Nick Rucci"]
   s.email       = 'nick@rucci.io'
   s.files       = ["lib/marigold.rb"]
